b3cfed2db621fd2e354b37a7f1d31db6ba7faff0
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-13 10:12:09 -0800

    Styling Form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// icon names at: https://oblador.github.io/react-native-vector-icons/
+
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
