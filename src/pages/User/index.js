c00b0e3955ae5bd2c247872b11521c262f18453b
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-13 18:18:07 -0800

    API data searching

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return <View />;
+  }
 }
-
-User.navigationOptions = {
-  title: 'Perfil do Usuário',
-};
