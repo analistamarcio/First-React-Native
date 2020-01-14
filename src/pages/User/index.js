3d48a0f1ba983b66f92d61ca8750193d3e3fa95b
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-13 18:01:20 -0800

    Doing navigation

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User({ navigation }) {
+  console.tron.log(navigation.getParam('user'));
   return <View />;
 }
+
+User.navigationOptions = {
+  title: 'Perfil do Usuário',
+};
