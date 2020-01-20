0cbf8c11d5a18050985af22d031198207672b451
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-20 10:25:19 -0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main';
 import User from './pages/User';
